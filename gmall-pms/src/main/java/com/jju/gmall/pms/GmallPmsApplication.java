6ccb96104275deca74944e486bdc700cd2594eea
myunyu <1577979477@qq.com>
2020-03-12 18:58:00 +0800

    配置dubbo和mybatisplus环境

--- gmall-pms/src/main/java/com/jju/gmall/pms/GmallPmsApplication.java
+++ gmall-pms/src/main/java/com/jju/gmall/pms/GmallPmsApplication.java
@@ -1,8 +1,18 @@
 package com.jju.gmall.pms;
 
+
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+    1、配置整合dubbo
+    2、配置整合MyBatisPlus
+
+ */
+@EnableDubbo        //开启dubbo的注解支持
+@MapperScan(basePackages = "com.jju.gmall.pms.mapper")
 @SpringBootApplication
 public class GmallPmsApplication {
 
--- gmall-pms/src/test/java/com/jju/gmall/pms/GmallPmsApplicationTests.java
+++ gmall-pms/src/test/java/com/jju/gmall/pms/GmallPmsApplicationTests.java
@@ -1,7 +1,10 @@
 package com.jju.gmall.pms;
 
+import com.jju.gmall.pms.entity.Product;
+import com.jju.gmall.pms.service.ProductService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class GmallPmsApplicationTests {
 
+    @Autowired
+    ProductService productService;
+
     @Test
     public void contextLoads() {
+
+        Product product = productService.getById(1);
+        System.out.println(product.getName());
     }
 
 }
